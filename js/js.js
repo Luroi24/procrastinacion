9644958aa189bc2130789cd310e8a4de80063a3a
Luis <luise.ascencio13@gmail.com>
2022-06-11 20:38:23 -0500

    más texto

--- js/js.js
+++ js/js.js
@@ -6,9 +6,12 @@ const progressSteps = document.querySelectorAll(".progress-step");
 const hasPartner = document.getElementById("der_cony_no");
 const allQuestions = document.querySelectorAll(".options");
 
+let indexProc = 0;
+
 for(i=0 ; i < allQuestions.length; i++){
 	for(j=0 ; j < 5; j++){
 		let txt =""
+		let val=""
 		const division = document.createElement("div");
 		division.className = "form-check form-check-inline";
 		allQuestions[i].appendChild(division);
@@ -18,14 +21,19 @@ for(i=0 ; i < allQuestions.length; i++){
 		divLabel.setAttribute("for",'OE'+(i+1)+'-'+(j+1));
 		switch(j){
 			case 0: txt = "Nunca";
+					val = 1;
 			break;
 			case 1: txt = "Casi nunca";
+					val = 2;
 			break;
 			case 2: txt = "A veces";
+					val = 3;
 			break;
 			case 3: txt = "Casi siempre";
+					val = 4;
 			break;
 			case 4: txt = "Siempre";
+					val = 5;
 			break;
 		}
 		const labeltext = document.createTextNode(txt);
@@ -37,6 +45,7 @@ for(i=0 ; i < allQuestions.length; i++){
 		divInput.setAttribute("id",'OE'+(i+1)+'-'+(j+1));
 		divInput.setAttribute("name",'OE'+(i+1));
 		divInput.setAttribute("required",'true');
+		divInput.setAttribute("value",val);
 
 		division.appendChild(divLabel);
 		division.appendChild(divInput);
@@ -49,6 +58,9 @@ nextBtns.forEach(btn =>{
 	btn.addEventListener("click", () => {
 		if(!checkVal()){
 			formStepsNum++;
+			if(formStepsNum == 2){
+				activateRes();
+			}
 			updateFormsSteps();
 			updateProgressBar();
 		}
@@ -68,7 +80,8 @@ prevBtns.forEach(btn =>{
 });
 
 function checkVal(){
-	let errors = 0, flag = 0;
+	let errors = 0;
+	indexProc = 0;
 	const campos = document.querySelectorAll(".form-step-active .needs-validation");
 	for( i=0 ; i < campos.length; i++){
 		const options = campos[i].querySelectorAll("input");
@@ -76,6 +89,7 @@ function checkVal(){
 			if(options[j].checked){
 				campos[i].classList.remove("is-invalid");
 				campos[i].classList.add("is-valid");
+				indexProc+=Number(options[j].value);
 				break;
 			}
 		}
@@ -85,6 +99,25 @@ function checkVal(){
 		}
 	}
 	return errors;
+}
+
+function activateRes(){
+	const indicator = (indexProc/105)*100;
+	if(indicator >0 && indicator <= 20 ){
+		document.getElementById("score-20").classList.remove("hidden");
+	}
+	else if(indicator > 20 && indicator <= 40){
+		document.getElementById("score-40").classList.remove("hidden");
+	}
+	else if(indicator > 40 && indicator <= 60){
+		document.getElementById("score-60").classList.remove("hidden");
+	}
+	else if(indicator > 60 && indicator <= 80){
+		document.getElementById("score-80").classList.remove("hidden");
+	}
+	else{
+		document.getElementById("score-100").classList.remove("hidden");
+	}
 }
 
 function updateFormsSteps(){
